61df5a8ccafa3eba4acb26c60cf280167a703ff3
gemtunde <gemtunde@gmail.com>
2025-03-12 18:32:28 +0100

    list page

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { backendUrl, currency } from "../../App";
+import { toast } from "react-toastify";
+import { MdRemove } from "react-icons/md";
 
-const List = () => {
-  return <div>List</div>;
+const List = ({ token }) => {
+  const [productList, setProductList] = React.useState([]);
+
+  const fetchProductList = async () => {
+    try {
+      const response = await axios.get(backendUrl + "/api/product/list", {
+        headers: { token },
+      });
+      if (response.data.success) {
+        setProductList(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+  useEffect(() => {
+    fetchProductList();
+  }, []);
+  return (
+    <div>
+      <p className="product-title">Product List</p>
+      <div className="product-list-container">
+        <div className="product-table-title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b className="action-title">Action</b>
+        </div>
+        {/* Product list */}
+        {productList.map((product) => (
+          <div className="product-row" key={product._id}>
+            <img src={product.image[0]} alt="" className="product-image" />
+            <p>{product.name}</p>
+            <p>{product.category}</p>
+            <p>
+              {currency}
+              {product.price}
+            </p>
+            <MdRemove className="product-action" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default List;
